44040c5398adcdff462bf0504c9739fc52601387
ss42 <sanjay.shrestha1988@gmail.com>
2016-10-24 21:22:12 -0700

    added few codes, still confused

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from cost_function_definition import f, f_prime, f_double_prime
 
 
 def minimize(f0, f1, f2, guess, epsilon):
-    print str(f0(guess)) + " is minimized at " + str(guess)
+    #print str(f0(guess)) + " is minimized at " + str(guess)
+    if f0 == 0:
+        return "f0 is zero"
+    if f1 == 0:
+        return "f1 is zero"
+    if f2 == 0:
+        return "f2 is zero"
+    while guess > epsilon:
+        guess = abs(f0 - f1/f2)
+    return guess
+
+
 
 
 minimize(f, f_prime, f_double_prime, 0, 0.000001)
