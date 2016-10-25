a73288da35066617784384e3714bb004f821bdd8
ss42 <sanjay.shrestha1988@gmail.com>
2016-10-25 15:05:47 -0700

    ..

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from cost_function_definition import f, f_prime, f_double_prime
 
 
 def minimize(f0, f1, f2, guess, epsilon):
+    #xn+1 = x0 - f'(x0)/f''(x0)
+
+
     #print str(f0(guess)) + " is minimized at " + str(guess)
-    if f0 == 0:
+    if f0(guess) == 0:
         return "f0 is zero"
-    if f1 == 0:
+    if f1(guess) == 0:
         return "f1 is zero"
-    if f2 == 0:
-        return "f2 is zero"
-    while guess > epsilon:
-        epsilon = guess - f1(guess)/f2(guess)
-        guess = abs(0 - f(guess))
+    if f2(guess) == 0:
+        return "undefined"
+    if guess > epsilon:
+        minimize(f1, f2, guess - f1(guess)/f2(guess), epsilon)
+    else:
+        return guess
+    print(guess)
     return guess
 
 
